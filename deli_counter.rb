26b54a26758607c0a8020edae2dc8b81d61dc677
Dominique De León <dominique.a.deleon@gmail.com>
2020-04-01 01:54:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,9 @@ def now_serving(katz_deli)
   else
     #puts "Currently serving #{katz_deli.shift}."
     katz_deli.each do |person|
-      name = katz_deli.shift
-      puts "Currently serving #{name}."
+    
+      puts "Currently serving #{person}."
+katz_deli.shift
       
     end
     
