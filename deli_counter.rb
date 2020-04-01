d5c5156bb75ee8d4cdc07ea39a710cf6cd4da402
Dominique De León <dominique.a.deleon@gmail.com>
2020-04-01 01:44:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,8 @@
 # Write your code here.
+def now_serving(katz_deli)
+  if katz_deli.size==0 
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving katz_deli.shift."
+  end
+end
